2e7383623d3240c1d210f81103c3028a8afb4342
LemonInc <l3moninc@gmail.com>
2018-06-22 00:00:54 +0100

    Setting up channel details page

--- Sapnin/Controller/ChannelDetailViewController.swift
+++ Sapnin/Controller/ChannelDetailViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ChannelDetailViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var imageArray = [UIImage(named: "selfie_image1"),UIImage(named: "selfie_image2"),UIImage(named: "selfie_image3"),UIImage(named: "selfie_image4")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationStyle()
@@ -23,4 +26,17 @@ class ChannelDetailViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
     }
     
+}
+
+extension ChannelDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ChannelDetailCollectionViewCell
+        cell.photo.image = imageArray[indexPath.row]
+        return cell
+    }
 }
---
+++ Sapnin/View/ChannelDetailCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelDetailCollectionViewCell.swift
+//  Sapnin
+//
+//  Created by Alan Lau on 21/06/2018.
+//  Copyright © 2018 lau. All rights reserved.
+//
+
+import UIKit
+
+class ChannelDetailCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var photo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
